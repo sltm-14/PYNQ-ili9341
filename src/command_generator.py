45e18f344cd224db762249b664692aee01583e93
sltm-14 <ie693242@iteso.mx>
2019-07-22 14:32:59 -0500

    Archivo de python para crear vetor que sea gregará a verilog, funciones de address_set, write_comm, write_data y clean_lcd hechas

--- src/command_generator.py
+++ src/command_generator.py
@@ -1,54 +1,47 @@
-address = "{LOW, LOW,  8'h2A},\n{LOW, HIGH, x1 >> 8},{LOW, HIGH, x1},{LOW, HIGH, x2 >> 8},{LOW, HIGH, x2},\n{LOW, LOW,  8'h2B},\n"
-address = address + "{LOW, HIGH, y1 >> 8},{LOW, HIGH, y1},{LOW, HIGH, y2 >> 8},{LOW, HIGH, y2},\n{LOW, LOW,  8'h2C},\n\n{LOW, LOW,  8'hFF}\n "
 
+def ItH(int_val):
+    hex_val = hex(int_val).lstrip("0x")
 
-# def lcd_write_data(data):
-#
-# def lcd_write_comm(comm):
+    if int_val == 0:
+        val = "8'h00"
+    elif int_val <16 :
+        val = "8'h0" + str(hex_val)
+    else:
+        val = "8'h" + str(hex_val)
+
+    return val
 
 def address_set(x1, y1,x2,y2):
-    print(" {LOW, LOW,  8'h2A},\n{LOW, HIGH, ",x1 >> 8,"},{LOW, HIGH, x1},{LOW, HIGH, x2 >> 8},{LOW, HIGH, x2},\n{LOW, LOW,  8'h2B},\n ")
+    add_set = ["\
+{LOW, LOW,  8'h2A},\n\
+{LOW, HIGH, ",ItH(x1 >> 8),"}, {LOW, HIGH, ",ItH(x1),"}, {LOW, HIGH, ",ItH(x2 >> 8),"}, {LOW, HIGH, ",ItH(x2),"},\n\
+{LOW, LOW,  8'h2B},\n\
+{LOW, HIGH, ",ItH(y1 >> 8),"}, {LOW, HIGH, ",ItH(y1),"}, {LOW, HIGH, ",ItH(y2 >> 8),"}, {LOW, HIGH, ",ItH(y2),"},\n\
+{LOW, LOW,  8'h2C},\n\n"]
 
-address_set(1250,5,7,9)
+    f.writelines(add_set)
 
 
+def Lcd_Write_Com(VH):
+    write_comm = ["\
+{LOW, LOW,  ",ItH(VH),"},\n"]
+    f.writelines(write_comm)
 
-# void Address_set(unsigned int x1,unsigned int y1,unsigned int x2,unsigned int y2){
-#   Lcd_Write_Com(0x2a);
-#
-#   Lcd_Write_Data(x1>>8);
-#   Lcd_Write_Data(x1);
-#   Lcd_Write_Data(x2>>8);
-#   Lcd_Write_Data(x2);
-#
-#   Lcd_Write_Com(0x2b);
-#
-#   Lcd_Write_Data(y1>>8);
-#   Lcd_Write_Data(y1);
-#   Lcd_Write_Data(y2>>8);
-#   Lcd_Write_Data(y2);
-#   Lcd_Write_Com(0x2c);
-# }
-#
-# void Lcd_Write_Com(unsigned char VH){
-#   *(portOutputRegister(digitalPinToPort(RS))) &=  ~digitalPinToBitMask(RS);//LCD_RS=0;
-#   Lcd_Writ_Bus(VH);
-# }
-#
-# void Lcd_Write_Data(unsigned char VH){
-#   *(portOutputRegister(digitalPinToPort(RS)))|=  digitalPinToBitMask(RS);//LCD_RS=1;
-#   Lcd_Writ_Bus(VH);
-# }
-#
-# void LCD_Clear(unsigned int j){
-#   unsigned int i,m;
-#   digitalWrite(CS,LOW);
-#   Address_set(0,0,240,320);
-#   for(i=0;i<240;i++)
-#     for(m=0;m<320;m++)
-#     {
-#       Lcd_Write_Data(j>>8);
-#       Lcd_Write_Data(j);
-#     }
-#   digitalWrite(CS,HIGH);
-# }
+
+def Lcd_Write_Data(VH):
+    write_comm = ["\
+{LOW, HIGH, ",ItH(VH),"},\n"]
+    f.writelines(write_comm)
+
+
+def LCD_Clear(j):
+    address_set(0,0,240,320)
+    for x in range(0,240):
+        for x in range(0,320):
+            Lcd_Write_Data(j>>8)
+            Lcd_Write_Data(j)
+
+
+f = open("text.txt", "w+")
+LCD_Clear(46083)
+f.close()
